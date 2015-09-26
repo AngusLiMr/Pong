bbcec02b75d60d35f6582be975dfeb7a375922dd
lang <gline80@163.com>
2015-09-26 18:16:36 +0800

    Initial Commit

---
+++ Pong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pong
+//
+//  Created by AngusLi on 15/9/26.
+//  Copyright (c) 2015年 AngusLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pong/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pong
+//
+//  Created by AngusLi on 15/9/26.
+//  Copyright (c) 2015年 AngusLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
